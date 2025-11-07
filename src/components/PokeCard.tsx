9870a2258c13582a457e08b17a16fd8b598f9787
Mario Kawakita <mario.jhk@gmail.com>
2025-11-07 15:19:22 -0300

    feat: pokemon page type | pokemon card style

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Card } from "./ui/card";
 import { Pokemon } from "@/models/Pokemon";
+import TypeTag from "./TypeTag";
 
 interface IPokeCard {
   pokemon: Pokemon;
@@ -8,19 +9,39 @@ interface IPokeCard {
 
 export default function PokeCard({ pokemon }: IPokeCard) {
   return (
-    <Card className="h-full">
-      <div className="flex">
-        <h4>{pokemon.id}</h4>
-        <h3>{pokemon.name}</h3>
+    <Card className={`h-full w-50 flex items-center p-2 bg-linear-to-br from-red-700 to-red-950`}>
+      <div className="flex justify-between items-center w-full gap-2">
+        <Card className="p-2 h-15 flex justify-center w-4/6 bg-slate-950">
+          <h3 className="text-transform: capitalize text-lg font-bold text-green-700">
+            {pokemon.name}
+          </h3>
+        </Card>
+        <div className="flex justify-center items-center rounded-full w-15 h-15 bg-linear-to-t from-zinc-50 from-35% to-red-500 p-1 border-2 border-zinc-950">
+          <h4 className="font-bold text-center">{`#${pokemon.id}`}</h4>
+        </div>
       </div>
 
-      <Image
-        loading="eager"
-        src={pokemon.sprites.front_default || ""}
-        alt={`Image of ${pokemon.name}`}
-        width={100}
-        height={200}
-      />
+      <Card className="w-full flex items-center border-16 border-zinc-400 py-0 bg-sky-100">
+        <Image
+          loading="eager"
+          src={pokemon.sprites.front_default || ""}
+          alt={`Image of ${pokemon.name}`}
+          width={100}
+          height={200}
+        />
+      </Card>
+
+      <Card className="py-1 gap-2 items-center w-full bg-slate-950">
+        <h3 className="font-bold text-green-700">Types</h3>
+        <div className="flex items-center gap-0.5">
+          {pokemon.types.map((type) => (
+            <TypeTag
+              type={type}
+              key={`${pokemon.name}-${type.slot}`}
+            />
+          ))}
+        </div>
+      </Card>
     </Card>
   );
 }
--- src/components/TypeTag.tsx
+++ src/components/TypeTag.tsx
@@ -1,11 +1,20 @@
+import { TYPES_COLORS } from "@/constants/pokemon";
+import { PokemonType } from "@/models/Pokemon";
+
 interface ITypeTag {
-  type: string;
+  type: PokemonType;
 }
 
 export default function TypeTag({ type }: ITypeTag) {
+  const typeName = type.type.name.toUpperCase();
+  const typeColor = typeName as keyof typeof TYPES_COLORS;
+  const colors = TYPES_COLORS[typeColor];
+
   return (
-    <div className="flex">
-      <div className="flex"></div>
+    <div
+      className={`flex justify-center ${colors.style} font-bold rounded-md w-full py-0.5 px-2 min-w-15`}
+    >
+      {typeName}
     </div>
   );
 }
